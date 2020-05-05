8e8804a4dab3be27be450c64e386183c29749405
IlyaIlichev <ilia.ilichev@mail.ru>
2020-05-05 13:36:43 +0700

    Edit makefie to run gtests. Add compilation artificians to gitignore.

--- test/test.cpp
+++ test/test.cpp
@@ -1,28 +1,33 @@
-#include "../src/misstake.h"
 #include "../src/draw.h"
 #include "../src/filework.h"
+#include "../src/misstake.h"
 #include "gtest/gtest.h"
+
+int misse = 0;
+int missm = 0;
+int missh = 0;
+int endg = 0;
 
 TEST(FileworkTest, MissingFile)
 {
-  int result = file_check("../src/wrong.txt", "../src/wrong.txt", "../src/wrong.txt");
+  int result = file_check("src/wrong.txt", "src/wrong.txt", "src/wrong.txt");
   int expected = -1;
   EXPECT_EQ(expected, result);
 }
 
 TEST(FileworkTest, EmptyFile)
 {
-  int result = file_check("empty.txt", "../src/word2.txt", "../src/word3.txt");
+  int result = file_check("test/empty.txt", "src/word2.txt", "src/word3.txt");
   int expected = 0;
   EXPECT_EQ(expected, result);
 }
 
 TEST(FileworkTest, FileCheckPassed)
 {
-  int num = file_check("../src/word1.txt", "../src/word2.txt", "../src/word3.txt");
-  int result = 0
+  int num = file_check("src/word1.txt", "src/word2.txt", "src/word3.txt");
+  int result = 0;
   if(num>0){
-    result = 1'
+    result = 1;
   }
   int expected = 1;
   EXPECT_EQ(expected, result);
